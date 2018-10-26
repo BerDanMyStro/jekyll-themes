81f0602838d5e6a1e7dec997dbefb902b4aba4e4
Béres Dániel <daniel.beres@nexum.hu>
2018-10-26 16:18:23 +0200

    base form and current section

--- js/public.js
+++ js/public.js
@@ -18,10 +18,46 @@ $(document).ready(function(){
     // >> Section Menu
 
     let sectionLink = $('.sectionNav__item a');
+    let no_sectionNav__items = $('.sectionNav__item').length;
+    let no_sections = $('.sectionWrapper section').length;
 
         // Create elements
         sectionLink.each(function(){
             $(this).html('<span class="sectionNo">'+ $(this).html() +'</span><span class="circle"><span><em></em></span><span><em></em></span></span>');
+        });
+
+        // Display current section
+
+        $(window).scroll(function (event) {
+            let scroll = $(window).scrollTop();
+
+            if ( scroll >= $('#section--no1').offset().top && scroll < $('#section--no2').offset().top ) {
+                $('.sectionNav__item:nth-child(2) a').removeClass('current');
+                $('.sectionNav__item:nth-child(3) a').removeClass('current');
+                $('.sectionNav__item:nth-child(4) a').removeClass('current');
+                $('.sectionNav__item:nth-child(1) a').addClass('current');
+            } else if(scroll >= $('#section--no2').offset().top && scroll < $('#section--no3').offset().top ){
+                $('.sectionNav__item:nth-child(1) a').removeClass('current');
+                $('.sectionNav__item:nth-child(3) a').removeClass('current');
+                $('.sectionNav__item:nth-child(4) a').removeClass('current');
+                $('.sectionNav__item:nth-child(2) a').addClass('current');
+            } else if(scroll >= $('#section--no3').offset().top && scroll < $('#section--no4').offset().top ){
+                $('.sectionNav__item:nth-child(1) a').removeClass('current');
+                $('.sectionNav__item:nth-child(2) a').removeClass('current');
+                $('.sectionNav__item:nth-child(4) a').removeClass('current');
+                $('.sectionNav__item:nth-child(3) a').addClass('current');
+            } else if(scroll >= $('#section--no4').offset().top){
+                $('.sectionNav__item:nth-child(1) a').removeClass('current');
+                $('.sectionNav__item:nth-child(2) a').removeClass('current');
+                $('.sectionNav__item:nth-child(3) a').removeClass('current');
+                $('.sectionNav__item:nth-child(4) a').addClass('current');
+            } else {
+                $('.sectionNav__item:nth-child(1) a').removeClass('current');
+                $('.sectionNav__item:nth-child(2) a').removeClass('current');
+                $('.sectionNav__item:nth-child(3) a').removeClass('current');
+                $('.sectionNav__item:nth-child(4) a').removeClass('current');
+            }
+
         });
 
     sectionLink.on('click', function(){
@@ -29,7 +65,6 @@ $(document).ready(function(){
         $('body, html').stop().animate({scrollTop:l}, 500);
         return false;
     });
-
 
     // Jump to top - mobilFooter
     $('.mobilFooter__item--jumpTo').on('click', function(){
@@ -86,6 +121,23 @@ $(document).ready(function(){
         sampleDark.css('background', colorDark);
 
         hexaData.children('.colorCard__data__hexa').text(colorLight + ' | ' + colorDark);
+
+    });
+
+    // Form validations
+    $('#baseForm').submit(function(e) {
+        e.preventDefault();
+        let first_name = $('#first_name').val();
+        let last_name = $('#last_name').val();
+
+        $(".error").remove();
+
+        if (first_name.length < 1) {
+            $('#first_name').after('<div class="error">This field is required!</div>');
+        }
+        if (last_name.length < 1) {
+            $('#last_name').after('<div class="error">This field is required!</div>');
+        }
 
     });
 
